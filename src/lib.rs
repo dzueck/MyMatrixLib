8190e32018e6cd3330b80faacfe66d1155f7f469
dzueck <dzueck@gmail.com>
2021-11-12 15:36:47 -0800

    Cleaned up VecN

--- src/lib.rs
+++ src/lib.rs
@@ -94,6 +94,14 @@ impl Index<usize> for Vec2 {
     }
 }
 
+impl From<VecN<2>> for Vec2 {
+    fn from(v: VecN<2>) -> Self {
+        Vec2::new(v[0], v[1])
+    }
+}
+
+
+
 
 #[derive(Clone, Copy, Debug)]
 pub struct Vec3 {
@@ -194,6 +202,11 @@ impl Index<usize> for Vec3 {
     }
 }
 
+impl From<VecN<3>> for Vec3 {
+    fn from(v: VecN<3>) -> Self {
+        Vec3::new(v[0], v[1], v[2])
+    }
+}
 
 
 
@@ -225,11 +238,6 @@ impl<const N: usize> VecN<N> {
         f32::sqrt(self.dot(self))
     }
     
-    pub fn cross(&self) -> VecN<N> {
-        let cross = [0.0; N];
-        Self::new(cross)
-    }
-
     pub fn dimension(&self) -> usize {
         N
     }
@@ -305,6 +313,25 @@ impl<const N: usize> Index<usize> for VecN<N> {
 
     fn index(&self, index: usize) -> &f32 {
         &self.vals[index]
+    }
+}
+
+impl<const N: usize> From<Vec2> for VecN<N> {
+    fn from(v2: Vec2) -> Self {
+        let mut new_vec = [0.0;N];
+        new_vec[0] = v2.x;
+        new_vec[1] = v2.y;
+        VecN::new(new_vec)
+    }
+}
+
+impl<const N: usize> From<Vec3> for VecN<N> {
+    fn from(v3: Vec3) -> Self {
+        let mut new_vec = [0.0;N];
+        new_vec[0] = v3.x;
+        new_vec[1] = v3.y;
+        new_vec[2] = v3.z;
+        VecN::new(new_vec)
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use my_vector_math::Vec3;
 use my_vector_math::VecN;
 
 fn main() {
-    let d = VecN::new([4.0;5]);
-    let x = VecN::new([4.0;5]);
+    let d = Vec2::new(1.0,0.0);
+    let x = Vec2::new(0.0,1.0);
 
-    println!("{:?}", d + x);
+    println!("{:?}", d.dot(x + d));
 }
 
